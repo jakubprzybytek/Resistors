9d2b95528f664300f7d587fe27915500117d3e7a
Jakub Przybytek <jakub.przybytek@gmail.com>
2026-07-06 12:25:02 +0000

    e2e tweaks

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, devices } from "@playwright/test";
 const baseURL = process.env.BASE_URL ?? "http://localhost:4173";
 const useLocalPreview = !process.env.BASE_URL;
 
+console.info(`[e2e] Playwright base URL: ${baseURL}`);
+
 export default defineConfig({
   testDir: "./tests/e2e",
   fullyParallel: true,
